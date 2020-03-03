1c97ec9d422fbb7e9a42a542bccd72bd90a90c02
Kukami <42496630+Kukami@users.noreply.github.com>
2020-03-03 15:26:08 +0100

    Create initial project files

---
+++ src/test/java/mops/rheinjug2/rheinjug2Tests.java
@@ -0,0 +1,13 @@
+package mops.rheinjug2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class rheinjug2Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
