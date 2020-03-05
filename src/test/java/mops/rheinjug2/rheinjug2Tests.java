0c86b939cb0990857741516b15af94ecb559acec
Timur Kelman <timur.kelman@wmdata.de>
2020-03-05 14:15:03 +0100

    add SuppressWarnings to checkstyle; format rheinjug2Tests.java accordingly

--- src/test/java/mops/rheinjug2/rheinjug2Tests.java
+++ src/test/java/mops/rheinjug2/rheinjug2Tests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SuppressWarnings("checkstyle:typename")
 class rheinjug2Tests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
