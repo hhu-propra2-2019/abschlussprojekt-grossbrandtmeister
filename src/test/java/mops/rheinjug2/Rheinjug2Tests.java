67f7abfb5c0c2f9b3159f9c7d3826d316c1b2e2f
Kukami <42496630+Kukami@users.noreply.github.com>
2020-03-05 14:59:08 +0100

    reformat and rename default test class to satisfy checkstyle

--- src/test/java/mops/rheinjug2/Rheinjug2Tests.java
+++ src/test/java/mops/rheinjug2/Rheinjug2Tests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class rheinjug2Tests {
+class Rheinjug2Tests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
