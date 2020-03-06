1dd27892c48cb4153041564ad070d9e9cecf01b8
Timur Kelman <timur.kelman@wmdata.de>
2020-03-06 15:05:11 +0100

    add messages to tests

--- src/test/java/mops/rheinjug2/meetupcom/MeetupComTest.java
+++ src/test/java/mops/rheinjug2/meetupcom/MeetupComTest.java
@@ -115,13 +115,17 @@ class MeetupComTest {
     var events = meetupcom.getRheinJugEventsSince(calendar);
     mockServer.verify();
 
-    assertNotNull(events);
-    assertEquals(events.size(), 1);
+    assertNotNull(events, "events is null");
+    assertEquals(events.size(), 1, "events size isn't 1");
     var event = events.get(0);
     // only check properties we are interested in
-    assertEquals(event.getId(), "269005066");
-    assertEquals(event.getName(), "EntwickelBar 6.0");
-    assertEquals(event.getTime(), new Date(1599895800000L));
-    assertEquals(event.getDuration(), Duration.ofMillis(27000000L));
+    assertEquals(event.getId(), "269005066",
+        "Id isn't as expected");
+    assertEquals(event.getName(), "EntwickelBar 6.0",
+        "Name isn't as expected");
+    assertEquals(event.getTime(), new Date(1599895800000L),
+        "Date isn't as expected");
+    assertEquals(event.getDuration(), Duration.ofMillis(27000000L),
+        "Duration isn't as expected");
   }
 }
