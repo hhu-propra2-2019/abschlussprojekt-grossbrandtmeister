294ba26d4859adaabfd7e00dc5a12c723441e497
amalb102 <mamx0999196330>
2020-03-06 14:30:01 +0100

    add_main_html_template

--- src/main/java/mops/rheinjug2/Controllers/Rheinjug2Controller.java
+++ src/main/java/mops/rheinjug2/Controllers/Rheinjug2Controller.java
@@ -1,4 +1,4 @@
-package mops.rheinjug2;
+package mops.rheinjug2.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/rheinjug2")
 public class Rheinjug2Controller {
 
-  @GetMapping("/events")
+  @GetMapping("/")
   public String getEvents() {
-    return "eventsoverview";
-  }
-
-  @GetMapping("/personalView")
-  public String getMyEvents() {
-    return "personalView";
+    return "index";
   }
 }
