317868c2eccd7f819a7bb7101d9e0b54857c6e6e
Luca Krahn <lord@Lucas-MacBook-Pro.local>
2020-03-09 14:36:21 +0100

    fix typo

--- src/main/java/mops/rheinjug2/fileupload/FileService.java
+++ src/main/java/mops/rheinjug2/fileupload/FileService.java
@@ -3,7 +3,6 @@ package mops.rheinjug2.fileupload;
 import io.minio.MinioClient;
 import java.io.File;
 import java.io.FileOutputStream;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +11,18 @@ public class FileService {
 
   MinioClient minioClient;
 
-  @Value("${minio.buckek.name}")
+  @Value("${minio.bucket.name}")
   String defaultBucketName;
 
   @Value("${minio.default.folder}")
   String defaultBaseFolder;
 
-  @Autowired
   public FileService(MinioClient minioClient) {
     this.minioClient = minioClient;
   }
 
   public void uploadFile(String name, byte[] content) {
     File file = new File("/tmp/" + name);
-    file.canWrite();
-    file.canRead();
     try {
       FileOutputStream iofs = new FileOutputStream(file);
       iofs.write(content);
