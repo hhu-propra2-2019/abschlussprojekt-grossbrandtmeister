32267c37a3a532246e5562db7f24b67f598587bd
Kukami <42496630+Kukami@users.noreply.github.com>
2020-03-10 14:19:08 +0100

    add login requirement for StudentController for a user with role 'studentin'

--- src/main/java/mops/rheinjug2/controllers/StudentController.java
+++ src/main/java/mops/rheinjug2/controllers/StudentController.java
@@ -1,32 +1,63 @@
 package mops.rheinjug2.controllers;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import mops.rheinjug2.AccountCreator;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Secured({"ROLE_studentin"})
 @RequestMapping("/rheinjug2/student")
 public class StudentController {
+
+  private final Counter authenticatedAccess;
+
+  public StudentController(MeterRegistry registry) {
+    authenticatedAccess = registry.counter("access.authenticated");
+  }
+
+  /**
+   * Event Übersicht für Studenten.
+   */
   @GetMapping("/events")
-  public String getEvents(Model m) {
+  public String getEvents(KeycloakAuthenticationToken token, Model model) {
+    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
+    authenticatedAccess.increment();
     return "student_events_overview";
   }
 
+  /**
+   * Übersicht der Events für die der aktuelle Student angemeldet war/ist.
+   */
   @GetMapping("/visitedevents")
-  public String getPersonal() {
-
+  public String getPersonal(KeycloakAuthenticationToken token, Model model) {
+    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
+    authenticatedAccess.increment();
     return "personalView";
   }
 
+  /**
+   * Formular zum Beantragen von Credit-Points.
+   */
   @GetMapping("/creditpoints")
-  public String getCreditPoints() {
-
+  public String getCreditPoints(KeycloakAuthenticationToken token, Model model) {
+    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
+    authenticatedAccess.increment();
     return "credit_points_apply";
   }
 
+  /**
+   * Formular zur Einreichung der Zusammenfassung.
+   */
   @GetMapping("/reportsubmit")
-  public String reportsubmit() {
+  public String reportsubmit(KeycloakAuthenticationToken token, Model model) {
+    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
+    authenticatedAccess.increment();
     return "report_submit";
   }
 }
