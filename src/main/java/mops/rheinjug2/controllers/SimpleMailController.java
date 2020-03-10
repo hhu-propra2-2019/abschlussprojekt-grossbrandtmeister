93df32e969efc5faeaf201f6449dd5b971732248
pascal2707 <pascal.meinhard@gmx.de>
2020-03-10 16:45:05 +0100

    extract pdf creation into own service

--- src/main/java/mops/rheinjug2/controllers/SimpleMailController.java
+++ src/main/java/mops/rheinjug2/controllers/SimpleMailController.java
@@ -2,16 +2,11 @@ package mops.rheinjug2.controllers;
 
 import com.sun.istack.ByteArrayDataSource;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDate;
 import javax.activation.DataHandler;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
-import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import mops.rheinjug2.email.CertificateService;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
@@ -22,10 +17,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/rheinjug2")
 public class SimpleMailController {
   
-  transient PDDocument pdfForm;
+  public final transient CertificateService certificateService;
   public final transient JavaMailSender emailSender;
   
-  public SimpleMailController(JavaMailSender emailSender) {
+  public SimpleMailController(CertificateService certificateService, JavaMailSender emailSender) {
+    this.certificateService = certificateService;
     this.emailSender = emailSender;
   }
   
@@ -45,7 +41,7 @@ public class SimpleMailController {
     // Write certificate to outputStream
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     // Dummy Values for testing purposes
-    createCertificatePdf(outputStream, "foo", "bar", "foo.bar@foobar.de");
+    certificateService.createCertificatePdf(outputStream, "foo", "bar", "foo.bar@foobar.de");
     byte[] bytes = outputStream.toByteArray();
     
     ByteArrayDataSource dataSource = new ByteArrayDataSource(bytes, "application/pdf");
@@ -69,58 +65,5 @@ public class SimpleMailController {
     emailSender.send(mimeMessage);
     
     return "Email Sent!";
-  }
-  
-  /**
-   * Füllt den Schein mit den Informationen des/der Studenten/Studentin.
-   *
-   * @param outputStream zum Speichern der befüllten PdfForm
-   * @param forename     des Studenten/der Studentin
-   * @param surname      des Studenten/der Studentin
-   * @param email        des Studenten/der Studentin
-   */
-  public void createCertificatePdf(ByteArrayOutputStream outputStream,
-                                   String forename, String surname, String email) {
-    
-    File pdf = new File("./DummyCertificate.pdf");
-    try {
-      pdfForm = PDDocument.load(pdf);
-      
-      PDDocumentCatalog docCatalog = pdfForm.getDocumentCatalog();
-      PDAcroForm acroForm = docCatalog.getAcroForm();
-      
-      setCertificateDate(acroForm);
-      acroForm.getField("name2[first]").setValue(forename);
-      acroForm.getField("name2[last]").setValue(surname);
-      acroForm.getField("email3").setValue(email);
-      acroForm.getField("scheinart6").setValue("EntwickelBar");
-      
-      // pdfForm.save("DummyCertificate" + forename + ".pdf");
-      pdfForm.save(outputStream);
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        pdfForm.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-  
-  /**
-   * Setzt beim Schein das aktuelle Datum.
-   *
-   * @param acroForm Schein-PDF
-   */
-  private static void setCertificateDate(PDAcroForm acroForm) throws IOException {
-    LocalDate currentDate = LocalDate.now();
-    int day = currentDate.getDayOfMonth();
-    int month = currentDate.getMonthValue();
-    int year = currentDate.getYear();
-    
-    acroForm.getField("date7[day]").setValue(Integer.toString(day));
-    acroForm.getField("date7[month]").setValue(Integer.toString(month));
-    acroForm.getField("date7[year]").setValue(Integer.toString(year));
   }
 }
