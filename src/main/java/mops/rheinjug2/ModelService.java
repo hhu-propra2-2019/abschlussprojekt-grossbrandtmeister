a502d981b0e153f3fb74df20683ff1948d8db2c8
Emilia Nenova <emilia.nenova1@gmail.com>
2020-03-11 15:57:37 +0100

    Add method findAllEvents

--- src/main/java/mops/rheinjug2/ModelService.java
+++ src/main/java/mops/rheinjug2/ModelService.java
@@ -1,6 +1,10 @@
 package mops.rheinjug2;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import mops.rheinjug2.entities.Event;
 import mops.rheinjug2.repositories.EventRepository;
 import mops.rheinjug2.repositories.StudentRepository;
 import org.springframework.stereotype.Service;
@@ -14,4 +18,14 @@ public class ModelService {
     this.studentRepository = studentRepository;
     this.eventRepository = eventRepository;
   }
+
+  public List<Event> getAllEvents() {
+    return StreamSupport.stream(eventRepository.findAll().spliterator(), false)
+        .collect(Collectors.toList());
+  }
+
+  private boolean studentIsPersisted() {
+
+  }
+
 }
