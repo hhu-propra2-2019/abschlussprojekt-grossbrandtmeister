8fcc45babb7912c691dba7bd181dff486a29fc96
Hanney100 <hannah.hengelbrock@hhu.de>
2020-03-17 17:51:13 +0100

    fix textfailure in SecurityConfigTests due to no access to MinIO throw try catch in getmapping(report_submit)

--- src/main/java/mops/rheinjug2/controllers/StudentController.java
+++ src/main/java/mops/rheinjug2/controllers/StudentController.java
@@ -2,16 +2,6 @@ package mops.rheinjug2.controllers;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.minio.errors.ErrorResponseException;
-import io.minio.errors.InsufficientDataException;
-import io.minio.errors.InternalException;
-import io.minio.errors.InvalidArgumentException;
-import io.minio.errors.InvalidBucketNameException;
-import io.minio.errors.InvalidResponseException;
-import io.minio.errors.NoResponseException;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import mops.rheinjug2.AccountCreator;
 import mops.rheinjug2.fileupload.FileService;
@@ -22,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.xmlpull.v1.XmlPullParserException;
 
 @Controller
 @Secured({"ROLE_studentin"})
@@ -73,15 +62,17 @@ public class StudentController {
    * Formular zur Einreichung der Zusammenfassung.
    * Das Summary-Objekt muss noch auf die Datenbank angepasst werden.
    */
+  @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
   @GetMapping("/reportsubmit")
-  public String reportsubmit(final KeycloakAuthenticationToken token, final Model model)
-      throws IOException, InvalidKeyException, NoSuchAlgorithmException, XmlPullParserException,
-      InvalidArgumentException, InvalidResponseException, InternalException, NoResponseException,
-      InvalidBucketNameException, InsufficientDataException, ErrorResponseException {
+  public String reportsubmit(final KeycloakAuthenticationToken token, final Model model) {
     final LocalDate today = LocalDate.now();
     final String eventname = "das coolste Event";
-    String content = fileService.getContentOfFileAsString("VorlageZusammenfassung.md");
-    if (content.isEmpty()) {
+    String content;
+    try {
+      content = fileService.getContentOfFileAsString("VorlageZusammenfassung.md");
+      content = content.isEmpty()
+          ? "Vorlage momentan nicht vorhanden. Schreib hier deinen Code hinein." : content;
+    } catch (final Exception e) {
       content = "Vorlage momentan nicht vorhanden. Schreib hier deinen Code hinein.";
     }
     final String student = "Hannah Hengelbrock";
