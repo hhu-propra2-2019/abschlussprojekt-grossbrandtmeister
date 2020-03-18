ce7d839b606297db8a31a08988ba44cdb68b151f
Kukami <42496630+Kukami@users.noreply.github.com>
2020-03-18 16:53:36 +0100

    Add test for refreshRheinjugEvents of EventService

--- src/test/java/mops/rheinjug2/services/EventServiceTests.java
+++ src/test/java/mops/rheinjug2/services/EventServiceTests.java
@@ -1,18 +1,25 @@
 package mops.rheinjug2.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import mops.rheinjug2.entities.Event;
+import mops.rheinjug2.meetupcom.MeetupCom;
+import mops.rheinjug2.meetupcom.Venue;
 import mops.rheinjug2.repositories.EventRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,10 +28,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith({MockitoExtension.class})
-//@ContextConfiguration(classes = {MeetupCom.class, MeetupComConfiguration.class})
+@ExtendWith(MockitoExtension.class)
 @DisplayName("Testing EventService")
 public class EventServiceTests {
+
+  @Mock
+  private transient mops.rheinjug2.meetupcom.Event meetupEvent;
+
+  @Mock
+  private transient Venue venue;
+
+  @Mock
+  private transient MeetupCom meetupComService;
 
   @Mock
   private transient EventRepository eventRepository;
@@ -32,25 +47,48 @@ public class EventServiceTests {
   @InjectMocks
   private transient EventService eventService;
 
-  /**
-   * Setzt ein Event mit ungültigem Status auf.
-   */
-  @BeforeEach
-  public void setUp() {
+  @Test
+  public void refreshEventsTest() throws MalformedURLException {
+    // Fake data for the event we get from our meetupComService
+    when(meetupEvent.getId()).thenReturn("1234");
+    when(meetupEvent.getName()).thenReturn("Test Event");
+    when(meetupEvent.getDescription()).thenReturn("Description");
+    when(meetupEvent.getTime()).thenReturn(Instant.ofEpochMilli(0));
+    when(meetupEvent.getUtcOffset()).thenReturn(Duration.ofMillis(0));
+    when(meetupEvent.getVenue()).thenReturn(venue);
+    when(venue.getAddress1()).thenReturn("Address");
+    when(venue.getCity()).thenReturn("City");
+    when(venue.getName()).thenReturn("Venue");
+    when(meetupEvent.getLink()).thenReturn(new URL("http://test.link/"));
+    when(meetupEvent.getStatus()).thenReturn(mops.rheinjug2.meetupcom.Event.Status.PAST);
+
+    // Add that fake Event to a List that gets returned by out meetupComService
+    final List<mops.rheinjug2.meetupcom.Event> eventList = new ArrayList<>();
+    eventList.add(meetupEvent);
+    when(meetupComService.getRheinJugEventsSince(any(LocalDateTime.class))).thenReturn(eventList);
+
+    eventService.refreshRheinjugEvents(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC));
+    verify(meetupComService, times(1))
+        .getRheinJugEventsSince(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC));
+    verify(eventRepository, times(1)).findEventByMeetupId("1234");
+    verify(eventRepository, times(1)).save(any(Event.class));
+    verify(eventRepository, times(1)).findEventsByStatus("UPCOMING");
+  }
+
+  @Test
+  public void updateEventStatusTest() {
+    // Create Event with test data and invalid status
     final Event testEvent = new Event();
     testEvent.setMeetupId("1234");
     testEvent.setTitle("Test Event");
     testEvent.setDate(LocalDateTime.now(ZoneId.of("Europe/Berlin")));
     testEvent.setStatus("UPCOMING");
 
+    // Add that test Event to list that gets returned by our repository
     final List<Event> eventList = new ArrayList<>();
     eventList.add(testEvent);
-
     when(eventRepository.findEventsByStatus(anyString())).thenReturn(eventList);
-  }
 
-  @Test
-  public void updateEventStatusTest() {
     final ArgumentCaptor<Event> argumentCaptor = ArgumentCaptor.forClass(Event.class);
     eventService.updateStatusOfPastEvents();
     verify(eventRepository, times(1)).findEventsByStatus("UPCOMING");
