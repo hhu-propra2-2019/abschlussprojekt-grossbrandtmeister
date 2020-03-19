7874ff20b5281a92138230f22949961c27f51a58
pascal2707 <pascal.meinhard@gmx.de>
2020-03-19 16:07:20 +0100

    modify CreditpointController.java and credit_points_apply.html
    Co-authored-by: Luca110 <luca10399@yahoo.de>

--- src/main/java/mops/rheinjug2/creditpoints/CreditpointController.java
+++ src/main/java/mops/rheinjug2/creditpoints/CreditpointController.java
@@ -2,9 +2,14 @@ package mops.rheinjug2.creditpoints;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import java.util.List;
 import mops.rheinjug2.Account;
 import mops.rheinjug2.AccountCreator;
 import mops.rheinjug2.ModelService;
+import mops.rheinjug2.email.EmailService;
+import mops.rheinjug2.entities.Event;
+import mops.rheinjug2.entities.Student;
+import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -22,20 +27,56 @@ public class CreditpointController {
   
   private final transient Counter authenticatedAccess;
   private final transient ModelService modelService;
+  private final transient EmailService emailService;
   private transient CertificateForm certificateForm = new CertificateForm();
   
   
-  public CreditpointController(final MeterRegistry registry, final ModelService modelService) {
+  @SuppressWarnings("checkstyle:MissingJavadocMethod")
+  public CreditpointController(MeterRegistry registry,
+                               ModelService modelService,
+                               EmailService emailService) {
     authenticatedAccess = registry.counter("access.authenticated");
     this.modelService = modelService;
+    this.emailService = emailService;
+  }
+  
+  
+  /**
+   * Methode die überprüft ob der/die Student/in Zusammenfassungen bei einer EntwickelBar
+   * abgegeben hat um CreditPoints zu beantragen.
+   */
+  @GetMapping("")
+  public String getCreditPoints(KeycloakAuthenticationToken token, Model model) {
+    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
+    authenticatedAccess.increment();
+    
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    String login = principal.getKeycloakSecurityContext().getIdToken().getName();
+    
+    //  False setzen
+    boolean eventsAreUsable = true;
+    
+    if (modelService.loadStudentByLogin(login) != null) {
+      Student student = modelService.loadStudentByLogin(login);
+      List<Event> allEventsForCP = modelService.getAllEventsForCP(login);
+      model.addAttribute("events", allEventsForCP);
+      eventsAreUsable = modelService.checkEventsForCertificate(login);
+    }
+    boolean disabled = !eventsAreUsable;
+    model.addAttribute("disabled", disabled);
+    
+    return "credit_points_apply";
+    // CertificateServer Veranstaltungen übergeben ODER überprüfung ob vorhanden
+    // Veranstaltungen auf jeden Fall auf gebucht setzen
+    // sendEmail (java)
   }
   
   /**
    * Get-Formular für die Scheinabgabe.
    */
-  @GetMapping("/certificateform")
-  public String formular(final KeycloakAuthenticationToken token,
-                         final Model model) {
+  @RequestMapping("/certificateform")
+  public String formular(KeycloakAuthenticationToken token,
+                         Model model) {
     model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
     authenticatedAccess.increment();
     model.addAttribute("certificateForm", certificateForm);
@@ -46,45 +87,23 @@ public class CreditpointController {
    * Post-Formular für die Scheinabgabe.
    */
   @PostMapping("certificateform")
-  public String formular(@ModelAttribute("certificateForm") final CertificateForm certificateForm,
-                         final KeycloakAuthenticationToken token,
-                         final Model model) {
-    final Account account = AccountCreator.createAccountFromPrincipal(token);
+  public String formular(@ModelAttribute("certificateForm") CertificateForm certificateForm,
+                         KeycloakAuthenticationToken token,
+                         Model model) {
+    Account account = AccountCreator.createAccountFromPrincipal(token);
     model.addAttribute("account", account);
     this.certificateForm = certificateForm;
     model.addAttribute("certificateForm", certificateForm);
     authenticatedAccess.increment();
+    
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    String login = principal.getKeycloakSecurityContext().getIdToken().getName();
+    Student student = modelService.loadStudentByLogin(login);
+    
     
     System.out.println(certificateForm.getGender() + certificateForm.getMatNr());
     return "credit_points_form";
   }
   
   
-  /**
-   * Methode die überprüft ob der/die Student/in Zusammenfassungen bei drei Abendveranstaltungen
-   * abgegeben hat um CreditPoints zu beantragen.
-   */
-  @RequestMapping("/eveningevents")
-  public void eveningEventsCP(final KeycloakAuthenticationToken token, final Model model) {
-    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
-    authenticatedAccess.increment();
-    
-    
-    // CertificateServer Veranstaltungen übergeben ODER überprüfung ob vorhanden
-    // Veranstaltungen auf jeden Fall auf gebucht setzen
-    // sendEmail (rheinjug)
-  }
-  
-  /**
-   * Methode die überprüft ob der/die Student/in Zusammenfassungen bei einer EntwickelBar
-   * abgegeben hat um CreditPoints zu beantragen.
-   */
-  @RequestMapping("/entwickelbar")
-  public void entwickelbarCP(final KeycloakAuthenticationToken token, final Model model) {
-    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
-    authenticatedAccess.increment();
-    // CertificateServer Veranstaltungen übergeben ODER überprüfung ob vorhanden
-    // Veranstaltungen auf jeden Fall auf gebucht setzen
-    // sendEmail (java)
-  }
 }
