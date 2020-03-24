9104890d0fb1d7716cc4d052f45e1affa8cad79b
Emilia Nenova <emilia.nenova1@gmail.com>
2020-03-24 16:48:30 +0100

    Fix StudentController Version

--- src/main/java/mops/rheinjug2/controllers/StudentController.java
+++ src/main/java/mops/rheinjug2/controllers/StudentController.java
@@ -5,16 +5,19 @@ import io.micrometer.core.instrument.MeterRegistry;
 import java.time.LocalDateTime;
 import mops.rheinjug2.Account;
 import mops.rheinjug2.AccountCreator;
+import mops.rheinjug2.entities.Event;
 import mops.rheinjug2.fileupload.FileService;
 import mops.rheinjug2.fileupload.Summary;
 import mops.rheinjug2.services.ModelService;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @Secured({"ROLE_studentin"})
@@ -28,6 +31,7 @@ public class StudentController {
 
   transient FileService fileService;
 
+  @SuppressWarnings("checkstyle:MissingJavadocMethod")
   public StudentController(final MeterRegistry registry, final FileService fileService,
                            final ModelService modelService) {
     this.fileService = fileService;
@@ -35,13 +39,21 @@ public class StudentController {
     authenticatedAccess = registry.counter("access.authenticated");
   }
 
+  @GetMapping("/")
+  public String studentBase() {
+    return "redirect:/rheinjug2/student/events/";
+  }
+
   /**
    * Event Übersicht für Studenten.
    */
   @GetMapping("/events")
   public String getEvents(final KeycloakAuthenticationToken token, final Model model) {
-    model.addAttribute("account", AccountCreator.createAccountFromPrincipal(token));
+    final Account account = AccountCreator.createAccountFromPrincipal(token);
+    model.addAttribute("account", account);
     model.addAttribute("events", modelService.getAllEvents());
+    model.addAttribute("studentRegisteredForEvent",
+        modelService.getAllEventIdsPerStudent(account.getName()));
     authenticatedAccess.increment();
     return "student_events_overview";
   }
@@ -53,8 +65,6 @@ public class StudentController {
   @GetMapping("/visitedevents")
   public String getPersonal(final KeycloakAuthenticationToken token, final Model model) {
     final Account account = AccountCreator.createAccountFromPrincipal(token);
-//  final long eventId = 1;
-//  modelService.addStudentToEvent(account.getName(), account.getEmail(), eventId);
     model.addAttribute("account", account);
     model.addAttribute("exists", modelService.studentExists(account.getName()));
     model.addAttribute("studentEvents", modelService.getAllEventsPerStudent(account.getName()));
@@ -71,9 +81,16 @@ public class StudentController {
   @GetMapping("/reportsubmit")
   public String reportSubmit(final KeycloakAuthenticationToken token, final Model model,
                              final Long eventId) {
+    if (eventId == null) {
+      return "redirect:rheinjug2/student/visitedevents";
+    }
     final LocalDateTime today = LocalDateTime.now();
     final Account account = AccountCreator.createAccountFromPrincipal(token);
-    final String eventname = modelService.loadEventById(eventId).getTitle();
+    final Event event = modelService.loadEventById(eventId);
+    if (event == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event ID not found!");
+    }
+    final String eventname = event.getTitle();
     String content;
     try {
       content = fileService.getContentOfFileAsString("VorlageZusammenfassung.md");
@@ -96,7 +113,7 @@ public class StudentController {
   @PostMapping("/events")
   public String addStudentToEvent(final String name, final String email, final Long eventId) {
     modelService.addStudentToEvent(name, email, eventId);
-    return "personalView";
+    return "redirect:/rheinjug2/student/visitedevents";
   }
 
 }
