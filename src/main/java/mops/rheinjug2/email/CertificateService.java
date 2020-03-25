590b817bd3282672fcc5083af311291b736c41a4
Luca110 <Luca10399@yahoo.de>
2020-03-25 13:41:22 +0100

    add '-' for better overview in pdf

--- src/main/java/mops/rheinjug2/email/CertificateService.java
+++ src/main/java/mops/rheinjug2/email/CertificateService.java
@@ -15,13 +15,13 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 public class CertificateService {
-  
+
   private final transient int numberOfRequiredEntwickelbarEvents = 1;
   private final transient int numberOfRequiredEveningEvents = 3;
-  
+
   private transient PDDocument pdfForm;
   private transient ByteArrayOutputStream outputStream;
-  
+
   /**
    * Füllt den Schein mit den Informationen des/der Studenten/Studentin.
    *
@@ -30,65 +30,65 @@ public class CertificateService {
    * @param matnr      Matrikelnummer des Studenten/der Studentin
    * @param usedEvents Liste der Events die für das PDF verbraucht werden.
    */
-  public byte[] createCertificatePdf(final String name, final String gender,
-                                     final String matnr, final List<Event> usedEvents) {
-    final File pdf = new File("./rheinjug_schein.pdf");
+  public byte[] createCertificatePdf(String name, String gender,
+                                     String matnr, List<Event> usedEvents) {
+    File pdf = new File("./rheinjug_schein.pdf");
     try {
       pdfForm = PDDocument.load(pdf);
-      
-      final PDDocumentCatalog docCatalog = pdfForm.getDocumentCatalog();
-      final PDAcroForm acroForm = docCatalog.getAcroForm();
-      
+
+      PDDocumentCatalog docCatalog = pdfForm.getDocumentCatalog();
+      PDAcroForm acroForm = docCatalog.getAcroForm();
+
       acroForm.getField("Vorname, Name").setValue(name);
-      
+
       acroForm.getField("Text").setValue(setGenderFormOfAddress(gender) + " "
           + name + " - Matrikelnummer " + matnr + " wird hiermit bescheinigt, dass "
           + setGenderPronoun(gender)
           + " an folgenden rheinjug-Veranstaltungen (Java in der Praxis) "
           + "teilgenommen und dazu jeweils eine Zusammenfassung geschrieben hat.");
-      
-      
+
+
       if (usedEvents.size() == numberOfRequiredEveningEvents) {
-        acroForm.getField("Veranstaltung 1").setValue(usedEvents.get(0).getTitle());
-        acroForm.getField("Veranstaltung 2").setValue(usedEvents.get(1).getTitle());
-        acroForm.getField("Veranstaltung 3").setValue(usedEvents.get(2).getTitle());
+        acroForm.getField("Veranstaltung 1").setValue("- " + usedEvents.get(0).getTitle());
+        acroForm.getField("Veranstaltung 2").setValue("- " + usedEvents.get(1).getTitle());
+        acroForm.getField("Veranstaltung 3").setValue("- " + usedEvents.get(2).getTitle());
       } else if (usedEvents.size() == numberOfRequiredEntwickelbarEvents) {
-        acroForm.getField("Veranstaltung 1").setValue(usedEvents.get(0).getTitle());
+        acroForm.getField("Veranstaltung 1").setValue("- " + usedEvents.get(0).getTitle());
         acroForm.getField("Veranstaltung 2").setValue("");
         acroForm.getField("Veranstaltung 3").setValue("");
       } else {
         return null;
       }
-      
+
       acroForm.getField("Datum 1").setValue(setCertificateDate());
       acroForm.getField("Datum 2").setValue(setCertificateDate());
-      
+
       outputStream = new ByteArrayOutputStream();
       pdfForm.save(outputStream);
-    } catch (final IOException e) {
+    } catch (IOException e) {
       log.catching(e);
     } finally {
       try {
         pdfForm.close();
-      } catch (final IOException e) {
+      } catch (IOException e) {
         log.catching(e);
       }
     }
     return outputStream.toByteArray();
   }
-  
+
   /**
    * Setzt beim Schein das aktuelle Datum.
    */
   private static String setCertificateDate() {
-    final LocalDate currentDate = LocalDate.now();
-    final int day = currentDate.getDayOfMonth();
-    final int month = currentDate.getMonthValue();
-    final int year = currentDate.getYear();
+    LocalDate currentDate = LocalDate.now();
+    int day = currentDate.getDayOfMonth();
+    int month = currentDate.getMonthValue();
+    int year = currentDate.getYear();
     return day + "." + month + "." + year;
   }
-  
-  private static String setGenderFormOfAddress(final String gender) {
+
+  private static String setGenderFormOfAddress(String gender) {
     switch (gender) {
       case "male":
         return "Herr";
@@ -98,8 +98,8 @@ public class CertificateService {
         return "";
     }
   }
-  
-  private static String setGenderPronoun(final String gender) {
+
+  private static String setGenderPronoun(String gender) {
     switch (gender) {
       case "male":
         return "er";
