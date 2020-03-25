3b59cf048a5110957b1e4f454bd8e99aad9a0c40
Hanney100 <hannah.hengelbrock@hhu.de>
2020-03-25 15:34:40 +0100

    fix spotbugs errors in test

--- src/test/java/mops/rheinjug2/fileupload/FileCheckServiceTest.java
+++ src/test/java/mops/rheinjug2/fileupload/FileCheckServiceTest.java
@@ -2,6 +2,7 @@ package mops.rheinjug2.fileupload;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +12,8 @@ class FileCheckServiceTest {
   @Test
   void checkIfIsMarkdown() {
     final MultipartFile testFile = new MockMultipartFile("file.md",
-        "file.md", "text/plain", "testdata".getBytes());
+        "file.md", "text/plain",
+        "testdata".getBytes(StandardCharsets.UTF_8));
 
     assertTrue(FileCheckService.isMarkdown(testFile));
   }
