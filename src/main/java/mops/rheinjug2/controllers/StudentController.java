75e971384100e5808908ccc99b0b3c7600ad4bc9
Luca110 <Luca10399@yahoo.de>
2020-03-26 15:16:06 +0100

    change cp into event type and price into email for jens
    Co-authored-by: pascal2707 <pascal.meinhard@gmx.de>

--- src/main/java/mops/rheinjug2/controllers/StudentController.java
+++ src/main/java/mops/rheinjug2/controllers/StudentController.java
@@ -24,13 +24,13 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/rheinjug2/student")
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public class StudentController {
-  
+
   private final transient Counter authenticatedAccess;
   private final transient ModelService modelService;
-  
-  
+
+
   transient FileService fileService;
-  
+
   @SuppressWarnings("checkstyle:MissingJavadocMethod")
   public StudentController(final MeterRegistry registry, final FileService fileService,
                            final ModelService modelService) {
@@ -38,12 +38,12 @@ public class StudentController {
     this.modelService = modelService;
     authenticatedAccess = registry.counter("access.authenticated");
   }
-  
+
   @GetMapping("/")
   public String studentBase() {
     return "redirect:/rheinjug2/student/events/";
   }
-  
+
   /**
    * Event Übersicht für Studenten.
    */
@@ -55,9 +55,16 @@ public class StudentController {
     model.addAttribute("studentRegisteredForEvent",
         modelService.getAllEventIdsPerStudent(account.getName()));
     authenticatedAccess.increment();
+
+    for (int i = 1; i < 25; i++) {
+      modelService.addStudentToEvent(account.getName(), account.getEmail(), (long) i);
+      modelService.submitSummary(account.getName(), (long) i);
+      modelService.acceptSummary((long) i, account.getName());
+    }
+
     return "student_events_overview";
   }
-  
+
   /**
    * Übersicht der Events für die der aktuelle Student angemeldet war/ist.
    * Die EventId muss später durch die richtige Id aus der Datenbank ersetzt werden.
@@ -71,7 +78,7 @@ public class StudentController {
     authenticatedAccess.increment();
     return "personalView";
   }
-  
+
   /**
    * Formular zur Einreichung der Zusammenfassung.
    * Das Summary-Objekt muss noch auf die Angaben des jeweiligen Events aus
@@ -106,7 +113,7 @@ public class StudentController {
     authenticatedAccess.increment();
     return "report_submit";
   }
-  
+
   /**
    * Fügt einen Studenten einem Event hinzu.
    */
