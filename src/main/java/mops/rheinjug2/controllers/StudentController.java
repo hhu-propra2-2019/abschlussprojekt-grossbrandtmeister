f7a358766bb783cb9243f2cd8ad029e53e10a183
Luca110 <33316900+Luca110@users.noreply.github.com>
2020-03-26 17:07:27 +0100

    Template refactor (#122)

--- src/main/java/mops/rheinjug2/controllers/StudentController.java
+++ src/main/java/mops/rheinjug2/controllers/StudentController.java
@@ -25,13 +25,13 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/rheinjug2/student")
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public class StudentController {
-  
+
   private final transient Counter authenticatedAccess;
   private final transient ModelService modelService;
-  
-  
+
+
   transient FileService fileService;
-  
+
   @SuppressWarnings("checkstyle:MissingJavadocMethod")
   public StudentController(final MeterRegistry registry, final FileService fileService,
                            final ModelService modelService) {
@@ -39,12 +39,12 @@ public class StudentController {
     this.modelService = modelService;
     authenticatedAccess = registry.counter("access.authenticated");
   }
-  
+
   @GetMapping("/")
   public String studentBase() {
     return "redirect:/rheinjug2/student/events/";
   }
-  
+
   /**
    * Event Übersicht für Studenten.
    */
@@ -56,9 +56,10 @@ public class StudentController {
     model.addAttribute("studentRegisteredForEvent",
         modelService.getAllEventIdsPerStudent(account.getName()));
     authenticatedAccess.increment();
+
     return "student_events_overview";
   }
-  
+
   /**
    * Übersicht der Events für die der aktuelle Student angemeldet war/ist.
    * Die EventId muss später durch die richtige Id aus der Datenbank ersetzt werden.
@@ -72,7 +73,7 @@ public class StudentController {
     authenticatedAccess.increment();
     return "personalView";
   }
-  
+
   /**
    * Formular zur Einreichung der Zusammenfassung.
    * Das Summary-Objekt muss noch auf die Angaben des jeweiligen Events aus
@@ -108,7 +109,7 @@ public class StudentController {
     authenticatedAccess.increment();
     return "report_submit";
   }
-  
+
   /**
    * Fügt einen Studenten einem Event hinzu.
    */
