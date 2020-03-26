b3825ed751ab491e2a91ae316edf5cd40df851d4
pascal2707 <pascal.meinhard@gmx.de>
2020-03-26 16:10:18 +0100

    add CreditpointServiceTest.java

---
+++ src/test/java/mops/rheinjug2/creditpoints/CreditpointServiceTest.java
@@ -0,0 +1,146 @@
+package mops.rheinjug2.creditpoints;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.mail.MessagingException;
+import mops.rheinjug2.email.EmailService;
+import mops.rheinjug2.entities.Event;
+import mops.rheinjug2.entities.Student;
+import mops.rheinjug2.services.ModelService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CreditpointServiceTest {
+  
+  ModelService modelMock;
+  EmailService emailMock;
+  
+  CreditpointService creditpointService;
+  
+  @BeforeEach
+  public void setUp() {
+    modelMock = mock(ModelService.class);
+    emailMock = mock(EmailService.class);
+    creditpointService = new CreditpointService(modelMock, emailMock);
+  }
+  
+  @Test
+  void sendMailIfPossibleRequirementsAreMetEveningEvents() throws MessagingException {
+    final String login = "student";
+    final String email = "test@test.test";
+    final String name = "test test";
+    final Student student = createStudent(login, email);
+    final Event event1 = createEvent("Event 1", "Abendveranstaltung");
+    final Event event2 = createEvent("Event 2", "Abendveranstaltung");
+    final Event event3 = createEvent("Event 3", "Abendveranstaltung");
+    final List<Event> events = new ArrayList<>();
+    events.add(event1);
+    events.add(event2);
+    events.add(event3);
+    final CertificateForm certificateForm = createCertificateForm("7654321", "male");
+    when(modelMock.loadStudentByLogin(login)).thenReturn(student);
+    when(modelMock.checkEventsForCertificate(login)).thenReturn(true);
+    when(modelMock.getEventsForCertificate(login)).thenReturn(events);
+    
+    creditpointService.sendMailIfPossible(certificateForm, login, name);
+    
+    verify(emailMock, times(1))
+        .sendMail(name, certificateForm.getGender(), certificateForm.getMatNr(), events);
+  }
+  
+  @Test
+  void sendMailIfPossibleRequirementsAreMetEntwickelbar() throws MessagingException {
+    final String login = "student";
+    final String email = "test@test.test";
+    final String name = "test test";
+    final Student student = createStudent(login, email);
+    final Event event1 = createEvent("Event 1", "EntwickelBar");
+    final List<Event> events = new ArrayList<>();
+    events.add(event1);
+    final CertificateForm certificateForm = createCertificateForm("1234567", "male");
+    when(modelMock.loadStudentByLogin(login)).thenReturn(student);
+    when(modelMock.checkEventsForCertificate(login)).thenReturn(true);
+    when(modelMock.getEventsForCertificate(login)).thenReturn(events);
+    
+    creditpointService.sendMailIfPossible(certificateForm, login, name);
+    
+    verify(emailMock, times(1))
+        .sendMail(name, certificateForm.getGender(), certificateForm.getMatNr(), events);
+  }
+  
+  @Test
+  void sendMailIfPossibleRequirementsAreNotMet() throws MessagingException {
+    final String login = "studentin";
+    final String email = "foo@bar.test";
+    final String name = "test test";
+    final Student student = createStudent(login, email);
+    final Event event1 = createEvent("Event 1", "Abendveranstaltung");
+    final List<Event> events = new ArrayList<>();
+    events.add(event1);
+    final CertificateForm certificateForm = createCertificateForm("1234567", "female");
+    when(modelMock.loadStudentByLogin(login)).thenReturn(student);
+    when(modelMock.checkEventsForCertificate(login)).thenReturn(false);
+    
+    creditpointService.sendMailIfPossible(certificateForm, login, name);
+    
+    verify(emailMock, never())
+        .sendMail(name, certificateForm.getGender(), certificateForm.getMatNr(), events);
+  }
+  
+  @Test
+  void buttonDoesntNeedToBeDisabled() {
+    final String login = "studentin";
+    final String email = "foo@bar.test";
+    final Student student = createStudent(login, email);
+    when(modelMock.loadStudentByLogin(login)).thenReturn(student);
+    when(modelMock.checkEventsForCertificate(login)).thenReturn(true);
+    
+    assertThat(creditpointService.checkIfButtonNeedsToBeDisabled(login)).isTrue();
+  }
+  
+  @Test
+  void buttonNeedsToBeDisabledBecauseTheStudentIsNotInDatabase() {
+    final String login = "studentin";
+    when(modelMock.loadStudentByLogin(login)).thenReturn(null);
+    
+    assertThat(creditpointService.checkIfButtonNeedsToBeDisabled(login)).isFalse();
+  }
+  
+  @Test
+  void buttonNeedsToBeDisabledBecauseThereArentEnoughUsableEvents() {
+    final String login = "studentin";
+    final String email = "foo@bar.test";
+    final Student student = createStudent(login, email);
+    
+    when(modelMock.loadStudentByLogin(login)).thenReturn(student);
+    when(modelMock.checkEventsForCertificate(login)).thenReturn(false);
+    
+    assertThat(creditpointService.checkIfButtonNeedsToBeDisabled(login)).isFalse();
+  }
+  
+  
+  private static Event createEvent(final String title, final String type) {
+    final Event event = new Event();
+    event.setTitle(title);
+    event.setType(type);
+    return event;
+  }
+  
+  private static Student createStudent(final String login, final String email) {
+    return new Student(login, email);
+  }
+  
+  private static CertificateForm createCertificateForm(final String matnr, final String gender) {
+    final CertificateForm certificateForm = new CertificateForm();
+    certificateForm.setMatNr(matnr);
+    certificateForm.setGender(gender);
+    return certificateForm;
+  }
+}
