68d03c8890f7f10f75126d49bc544b768354df6b
Hanney100 <hannah.hengelbrock@hhu.de>
2020-03-27 16:20:05 +0100

    change minio from port 9001 to 9000

--- src/main/java/mops/rheinjug2/DatabaseInitializer.java
+++ src/main/java/mops/rheinjug2/DatabaseInitializer.java
@@ -111,6 +111,13 @@ public class DatabaseInitializer implements ServletContextInitializer {
         event.setStatus("PAST");
         event.setType("Entwickelbar");
       }
+      if (event.getId() == 3) {
+        event.setDuration(Duration.ofHours(1));
+        event.setDate(LocalDateTime.of(LocalDate.of(2020, 3, 21), LocalTime.of(17, 30)));
+        event.setDeadline(event.getDate().plus(event.getDuration()).plusDays(7));
+        event.setStatus("PAST");
+        event.setType("Entwickelbar");
+      }
       eventRepository.save(event);
     });
   }
